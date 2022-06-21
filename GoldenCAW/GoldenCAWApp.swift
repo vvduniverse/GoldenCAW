1769c293c5e86e641acf2b01332fa6d29188bdd5
vvduniverse <86676363+vvduniverse@users.noreply.github.com>
2022-06-21 13:53:20 +0300

    Initial Commit

---
+++ GoldenCAW/GoldenCAWApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoldenCAWApp.swift
+//  GoldenCAW
+//
+//  Created by Vahtee Boo on 21.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct GoldenCAWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
